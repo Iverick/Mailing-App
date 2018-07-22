8edbded9ff113b79357dbb6d79b23d82759c915a
Pawel Goloblia <iverick091@gmail.com>
2018-07-22 13:53:45 +0300

    Added two tests for a creation of Subscriber model instances.

--- django/mailinglist/tests.py
+++ django/mailinglist/tests.py
@@ -1,3 +1,65 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from unittest.mock import patch
+
+from mailinglist.factories import SubscriberFactory
+from mailinglist.models import (MailingList, Subscriber,)
+
+
+class MockSendEmailToSubscriberTask:
+
+    def setUp(self):
+        self.send_confirmation_email_patch = patch(
+            'mailinglist.tasks.send_confirmation_email_to_subscriber'
+        )
+        self.send_confirmation_email_mock = self.send_confirmation_email_patch.start()
+        super().setUp()
+
+    def tearDown(self):
+        self.send_confirmation_email_patch.stop()
+        self.send_confirmation_email_mock = None
+        super().tearDown()
+
+
+class SubscriberCreationTestCase(MockSendEmailToSubscriberTask, TestCase):
+
+    def test_calling_create_queues_confirmation_email_task(self):
+        user = get_user_model().objects.create_user(
+            username='unit test runner'
+        )
+        mailing_list = MailingList.objects.create(
+            name='unit test',
+            owner=user,
+        )
+        Subscriber.objects.create(
+            email='unittest@example.com',
+            mailing_list=mailing_list,
+        )
+        self.assertEqual(
+            self.send_confirmation_email_mock.delay.call_count,
+            1
+        )
+
+
+class SubscriberManagerTestCase(TestCase):
+
+    def test_confirmed_subscribers_for_mailing_list(self):
+        mailing_list = MailingList.objects.create(
+            name='unit test',
+            owner=get_user_model().objects.create_user(username='unit test')
+        )
+        confirmed_users = [
+            SubscriberFactory(confirmed=True, mailing_list=mailing_list)
+            for n in range (3)
+        ]
+        unconfirmed_users = [
+            SubscriberFactory(mailing_list=mailing_list) for n in range (3)
+        ]
+        confirmed_users_qs = Subscriber.objects.confirmed_subscribers_for_mailing_list(
+            mailing_list=mailing_list
+        )
+        self.assertEqual(len(confirmed_users), confirmed_users_qs.count())
+
+        for user in confirmed_users_qs:
+            self.assertIn(user, confirmed_users)
